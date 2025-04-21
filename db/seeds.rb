bae869d4ce5536b77ec9cea56f66d0cfd3e0a4f9
Eli Herrera <esrbastianherrera@gmail.com>
2025-04-21 16:27:15 -0600

    feat(seeds): add participants and assignments to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Participants
+participants = [
+  { name: "Galilea", gender: :female },
+  { name: "Sebastian", gender: :male },
+  { name: "Alex", gender: :male }
+]
+participants.each { |participant| Participant.find_or_create_by!(participant) }
+
+# Assignments
+assignments = [
+  {
+
+    name: "Assignment 1",
+    description: "This is the first assignment.",
+    allowed_gender: :both
+  },
+  {
+    name: "Assignment 2",
+    description: "This is the second assignment.",
+    allowed_gender: :male
+  },
+  {
+
+    name: "Assignment 3",
+    description: "This is the third assignment.",
+    allowed_gender: :female
+  }
+]
+assignments.each { |assignment| Assignment.find_or_create_by!(assignment) }
