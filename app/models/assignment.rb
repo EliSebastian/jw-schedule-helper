8d008caa087a12935f3431d95de8fb93f1369723
Eli Herrera <esrbastianherrera@gmail.com>
2025-04-21 16:34:41 -0600

    feat(participant_assignment): update Assignment and Participant models with associations

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,6 @@
 class Assignment < ApplicationRecord
   enum :allowed_gender, { male: 0, female: 1, both: 2 }, validate: true
+
+  has_many :participant_assignments, dependent: :destroy
+  has_many :eligible_participants, through: :participant_assignments, source: :participant
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,5 +1,8 @@
 class Participant < ApplicationRecord
-  validates :name, :gender, presence: true
-
   enum :gender, { male: 0, female: 1 }, validate: true
+
+  has_many :participant_assignments, dependent: :destroy
+  has_many :allowed_assignments, through: :participant_assignments, source: :assignment
+
+  validates :name, :gender, presence: true
 end
