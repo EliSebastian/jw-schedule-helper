2e97a80346065f298c40477581430fea822c143c
Eli Herrera <esrbastianherrera@gmail.com>
2025-04-21 16:28:46 -0600

    feat(participant_assignments): create table with foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_21_215219) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_21_222730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,10 +22,22 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_21_215219) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "participant_assignments", force: :cascade do |t|
+    t.bigint "participant_id", null: false
+    t.bigint "assignment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["assignment_id"], name: "index_participant_assignments_on_assignment_id"
+    t.index ["participant_id"], name: "index_participant_assignments_on_participant_id"
+  end
+
   create_table "participants", force: :cascade do |t|
     t.string "name", null: false
     t.integer "gender", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "participant_assignments", "assignments"
+  add_foreign_key "participant_assignments", "participants"
 end
