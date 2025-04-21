730db1b70d542687be66734b2bdc774fb2b9a845
Eli Herrera <esrbastianherrera@gmail.com>
2025-04-21 15:47:11 -0600

    feat(assigments): add table

---
+++ db/migrate/20250421214215_create_assignments.rb
@@ -0,0 +1,11 @@
+class CreateAssignments < ActiveRecord::Migration[8.0]
+  def change
+    create_table :assignments do |t|
+      t.string :name
+      t.text :description
+      t.integer :allowed_gender
+
+      t.timestamps
+    end
+  end
+end
